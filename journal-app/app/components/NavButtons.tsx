227187c1ce81e25c0ccfe79587d9894a37c509e5
Tymee Kong <tymeekong562@gmail.com>
2023-11-15 15:13:52 -0800

    Added Welcome to navbar

--- journal-app/app/components/NavButtons.tsx
+++ journal-app/app/components/NavButtons.tsx
@@ -3,27 +3,26 @@ import React from 'react';
 import Link from 'next/link';
 import { auth } from '@/firebase-config';
 import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/navigation';
 
 const NavButtons = () => {
     const [user, loading, error] = useAuthState(auth);
     const [signOut, loadSignout, signOuterror] = useSignOut(auth);
-
-    if (loadSignout) {
-    }
+    const route = useRouter();
 
     return (
         <>
             {user ? (
                 <>
-                    {' '}
                     <button className='btn btn-ghost'>
-                        <Link href='/login'> {user.displayName} </Link>
+                        <Link href='/login'>Welcome {user.displayName}! </Link>
                     </button>
                     <button
                         className='btn btn-ghost'
                         onClick={async () => {
                             const success = await signOut();
                             if (success) alert('You are signed out');
+                            route.push('/');
                         }}
                     >
                         Sign out
