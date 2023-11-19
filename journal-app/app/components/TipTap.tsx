5fcfb21940003f68278bf0f6ec3462ae66dddc01
Tymee Kong <tymeekong562@gmail.com>
2023-11-19 08:14:15 -0800

    Added styling to tiptap

--- journal-app/app/components/TipTap.tsx
+++ journal-app/app/components/TipTap.tsx
@@ -9,6 +9,8 @@ import Superscript from '@tiptap/extension-superscript';
 import SubScript from '@tiptap/extension-subscript';
 import Heading from '@tiptap/extension-heading';
 import { useState } from 'react';
+import { GrBold, GrItalic, GrUnderline } from 'react-icons/gr';
+import { FaBold, FaItalic, FaUnderline } from 'react-icons/fa';
 
 const content =
     '<h2 style="text-align: center;">Welcome to Mantine rich text editor</h2><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
@@ -37,8 +39,14 @@ const TipTap = () => {
 
     return (
         <>
-            <EditorContent editor={editor}>
-                <div className='flex justify-center items-center padding-5'>
+            <EditorContent
+                editor={editor}
+                className='max-w-screen-md border border-primary bg-white  min-w-[768px] pl-5 pr-5 text-primary-content'
+            >
+                <div
+                    className='flex justify-center items-center padding-5 
+                    bg-secondary-content mb-5 -ml-5 -mr-5 '
+                >
                     <button
                         onClick={() =>
                             editor?.chain().focus().toggleBold().run()
@@ -46,9 +54,21 @@ const TipTap = () => {
                         disabled={
                             !editor?.can().chain().focus().toggleBold().run()
                         }
-                        className='m-5'
+                        className={
+                            editor?.isActive('bold')
+                                ? 'm-2 rounded border-4 border-primary p-2 text-primary'
+                                : 'm-1 rounded border-2 border-primary p-2 text-primary'
+                        }
                     >
-                        Bold
+                        {editor?.isActive('bold') ? (
+                            <>
+                                <FaBold />
+                            </>
+                        ) : (
+                            <>
+                                <GrBold />
+                            </>
+                        )}
                     </button>
                     <button
                         onClick={() =>
@@ -57,8 +77,21 @@ const TipTap = () => {
                         disabled={
                             !editor?.can().chain().focus().toggleItalic().run()
                         }
+                        className={
+                            editor?.isActive('italic')
+                                ? 'm-2 rounded border-4 border-primary p-3'
+                                : 'm-1 rounded border-2 border-primary p-2'
+                        }
                     >
-                        Italics
+                        {editor?.isActive('italic') ? (
+                            <>
+                                <FaItalic />
+                            </>
+                        ) : (
+                            <>
+                                <GrItalic />
+                            </>
+                        )}
                     </button>
                     <button
                         onClick={() =>
@@ -67,8 +100,21 @@ const TipTap = () => {
                         disabled={
                             !editor?.can().chain().toggleUnderline().run()
                         }
+                        className={
+                            editor?.isActive('underline')
+                                ? 'm-2 rounded border-4 border-primary p-3'
+                                : 'm-1 rounded border-2 border-primary p-2'
+                        }
                     >
-                        Underline
+                        {editor?.isActive('underline') ? (
+                            <>
+                                <FaUnderline />
+                            </>
+                        ) : (
+                            <>
+                                <GrUnderline />
+                            </>
+                        )}
                     </button>
                 </div>
             </EditorContent>
