6cde5810302971caa02d55d3788913f710f11f86
Tymee Kong <tymeekong562@gmail.com>
2023-11-20 13:56:49 -0800

    Added icons for text alignment

--- journal-app/app/components/TipTap.tsx
+++ journal-app/app/components/TipTap.tsx
@@ -11,6 +11,11 @@ import Heading from '@tiptap/extension-heading';
 import { useState } from 'react';
 import { GrBold, GrItalic, GrUnderline } from 'react-icons/gr';
 import { FaBold, FaItalic, FaUnderline } from 'react-icons/fa';
+import {
+    CiTextAlignCenter,
+    CiTextAlignLeft,
+    CiTextAlignRight,
+} from 'react-icons/ci';
 
 const content: string =
     '<h2 style="text-align: center;">Welcome to Mantine rich text editor</h2><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
@@ -47,7 +52,7 @@ const TipTap = () => {
             >
                 <div
                     className='flex justify-center items-center padding-5 
-                    bg-secondary-content mb-5 -ml-5 -mr-5 gap-x-5 '
+                    bg-secondary-content mb-5 -ml-5 -mr-5 gap-x-10 '
                 >
                     <div className='join'>
                         <button
@@ -145,7 +150,7 @@ const TipTap = () => {
                                     : 'btn join-item'
                             }
                         >
-                            Left
+                            <CiTextAlignLeft />
                         </button>
                         <button
                             onClick={() =>
@@ -161,7 +166,7 @@ const TipTap = () => {
                                     : 'btn join-item'
                             }
                         >
-                            Center
+                            <CiTextAlignCenter />
                         </button>
                         <button
                             onClick={() =>
@@ -177,7 +182,7 @@ const TipTap = () => {
                                     : 'btn join-item'
                             }
                         >
-                            Right
+                            <CiTextAlignRight />
                         </button>
                     </div>
                 </div>
