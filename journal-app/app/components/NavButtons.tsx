0853026b5988ce27c7c77a4095e07e045ea871a7
Tymee Kong <tymeekong562@gmail.com>
2023-11-21 02:59:38 -0800

    Fixed issue where users were logged out when hitting back button

--- journal-app/app/components/NavButtons.tsx
+++ journal-app/app/components/NavButtons.tsx
@@ -1,18 +1,33 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth } from '@/firebase-config';
-import { useSignOut, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import {
+    useSignOut,
+    useSignInWithGoogle,
+    useIdToken,
+} from 'react-firebase-hooks/auth';
 
 const NavButtons = () => {
     const [signOut, loadSignout, signOuterror] = useSignOut(auth);
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [userId, idLoading, idError] = useIdToken(auth);
+
+    // if (loading) {
+    //     return <div>Loading</div>;
+    // }
+    if (idLoading) {
+        return <div>Loading</div>;
+    }
+    if (error) {
+        console.error(error);
+    }
 
     return (
         <>
-            {user ? (
+            {userId ? (
                 <>
                     <button className='btn btn-ghost'>
-                        Welcome {user.user.displayName}!
+                        Welcome {userId.displayName}!
                     </button>
                     <button
                         className='btn btn-ghost'
@@ -26,7 +41,6 @@ const NavButtons = () => {
                 </>
             ) : (
                 <>
-                    {' '}
                     <button
                         className='btn btn-ghost'
                         onClick={() => signInWithGoogle()}
