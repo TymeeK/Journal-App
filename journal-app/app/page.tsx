692ad1b0cb4258f075c625ffb7521cbba5f672c5
Tymee Kong <tymeekong562@gmail.com>
2023-12-02 16:52:25 -0800

    Removed zustand

--- journal-app/app/page.tsx
+++ journal-app/app/page.tsx
@@ -1,7 +1,6 @@
 import { MantineProvider } from '@mantine/core';
 import TipTap from './components/TipTap';
 import HomePage from './components/Home';
-import { useJournalStore } from './zustand';
 
 export default function Home() {
     return (
